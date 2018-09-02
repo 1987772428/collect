06a74b32898c39aca31acfd6c07ca05ca8c2015d
1987772428 <1987772428@qq.com>
2018-09-02 21:03:06 +0800

    开奖号码采集，mybatis自动生成代码（mapper，mapper.xml，model），多线程定时任务
    号码来源：
    
      168：北京快乐8，北京赛车，重庆时时彩，广东十分
    
      官网：幸运飞艇，重庆十分（幸运农场），广东十一选五，上海时时乐，广西十分

---
+++ src/main/java/com/zu/collect/ScheduledTask.java
@@ -0,0 +1,106 @@
+package com.zu.collect;
+
+import com.zu.collect.controller.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ScheduledTask {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    BjknController bjknController;
+
+    @Autowired
+    BjpkController bjpkController;
+
+    @Autowired
+    XyftController xyftController;
+
+    @Autowired
+    CqsfController cqsfController;
+
+    @Autowired
+    CqController cqController;
+
+    @Autowired
+    GdsfController gdsfController;
+
+    @Autowired
+    ShsfController shsfController;
+
+    @Autowired
+    Gd11Controller gd11Controller;
+
+    @Autowired
+    GxsfController gxsfController;
+
+    // @Scheduled(initialDelay=1000, fixedDelay=30000)
+    @Scheduled(cron = "${scheduled.cron.bjkn}")
+    public void scheduleTaskBjkn()
+    {
+
+        logger.info("开始采集北京快乐8号码");
+        bjknController.collect();
+    }
+
+    @Scheduled(cron="${scheduled.cron.bjpk}")
+    public void scheduleTaskBjpk()
+    {
+        logger.info("开始采集北京pk10号码");
+        bjpkController.collect();
+    }
+
+    @Scheduled(cron="${scheduled.cron.xyft}")
+    public void scheduleTaskXyft()
+    {
+        logger.info("开始采集幸运飞艇号码");
+        xyftController.collect();
+    }
+
+    @Scheduled(cron="${scheduled.cron.cqsf}")
+    public void scheduleTaskCqsf()
+    {
+        logger.info("开始采集重庆十分（幸运农场）号码");
+        cqsfController.collect();
+    }
+
+    @Scheduled(cron="${scheduled.cron.cq}")
+    public void scheduleTaskCq()
+    {
+        logger.info("开始采集重庆时时彩号码");
+        cqController.collect();
+    }
+
+    @Scheduled(cron="${scheduled.cron.gdsf}")
+    public void scheduleTaskGdsf()
+    {
+        logger.info("开始采集广东十分号码");
+        gdsfController.collect();
+    }
+
+    @Scheduled(cron="${scheduled.cron.shsf}")
+    public void scheduleTaskShsf()
+    {
+        logger.info("开始采集上海时时乐号码");
+        shsfController.collect();
+    }
+
+    @Scheduled(cron="${scheduled.cron.gd11}")
+    public void scheduleTaskGd11()
+    {
+        logger.info("开始采集广东11选5号码");
+        gd11Controller.collect();
+    }
+
+    @Scheduled(cron="${scheduled.cron.gxsf}")
+    public void scheduleTaskGxsf()
+    {
+        logger.info("开始采集广西十分号码");
+        gxsfController.collect();
+    }
+}
